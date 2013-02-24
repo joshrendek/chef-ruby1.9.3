3e26e5fbaf2a4f991f2638fe0a08459267f32f8d
Josh Rendek <josh@bluescripts.net>
2013-02-23 21:43:18 -0500

    first

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'ruby1.9.3'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Josh Rendek'
+maintainer_email 'josh@joshrendek.com'
 license          'All rights reserved'
 description      'Installs/Configures ruby1.9.3'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))

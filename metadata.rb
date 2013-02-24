4758cb21ddf61adb552ffe702265d1dd6a7c2458
Josh Rendek <josh@bluescripts.net>
2013-02-23 21:51:54 -0500

    remove stuff

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,4 @@ maintainer       'Josh Rendek'
 maintainer_email 'josh@joshrendek.com'
 license          'All rights reserved'
 description      'Installs/Configures ruby1.9.3'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
